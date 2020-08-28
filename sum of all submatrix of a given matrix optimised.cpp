9532c7133152830dc73fa00360d4314ac79eaa43
Shivansh Dwivedi <44198084+lazyperfectionist@users.noreply.github.com>
2020-08-28 06:45:13 +0530

    Update sum of all submatrix of a given matrix optimised.cpp

--- sum of all submatrix of a given matrix optimised.cpp
+++ sum of all submatrix of a given matrix optimised.cpp
@@ -1,44 +1,46 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n=3;
-    int sum=0;
-    int arr[][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int pref_arr[3][3] = {0};
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    int r,c;
+    cin>>r>>c;
+    long long int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
+    int sum=0;
+    int pref_arr[3][3] = {0};
     cout<<endl;
     pref_arr[0][0] = arr[0][0];
-    for(int j=1;j<n;j++){
+    for(int j=1;j<c;j++){
         pref_arr[0][j] = pref_arr[0][j-1] + arr[0][j];
     }
-    for(int i=1;i<n;i++){
+    for(int i=1;i<r;i++){
         pref_arr[i][0] = pref_arr[i-1][0] + arr[i][0];
     }
-    for(int i=1;i<n;i++){
-        for(int j=1;j<n;j++){
+    for(int i=1;i<r;i++){
+        for(int j=1;j<c;j++){
             pref_arr[i][j] = pref_arr[i-1][j] + pref_arr[i][j-1] + arr[i][j] - pref_arr[i-1][j-1];
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
             cout<<pref_arr[i][j]<<" ";
         }
         cout<<endl;
     }
     //Two loops for fetching ith and jth index for top left
-    for(int tli=0;tli<n;tli++)
+    for(int tli=0;tli<r;tli++)
     {
-        for(int tlj=0;tlj<n;tlj++)
+        for(int tlj=0;tlj<c;tlj++)
         {
       //Two loops for fetching ith and jth index for bottom right
-            for(int bri=tli;bri<n;bri++)
+            for(int bri=tli;bri<r;bri++)
             {
-                for(int brj=tlj;brj<n;brj++)
+                for(int brj=tlj;brj<c;brj++)
                 {
                    //sum = sum + pref_arr[bri][brj] - pref_arr[bri][tlj-1] - pref_arr[tli-1][brj] + pref_arr[tli-1][tlj-1];
                     sum+=pref_arr[bri][brj];
