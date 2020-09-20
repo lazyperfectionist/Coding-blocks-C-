84178bd7b70b9f7ff31b80b10f3d0e07d98b5901
Shivansh Dwivedi <44198084+lazyperfectionist@users.noreply.github.com>
2020-09-21 01:30:59 +0530

    Update rotate_array.cpp

--- Algorithm/rotate_array.cpp
+++ Algorithm/rotate_array.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -10,4 +11,18 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<coins[i]<<" ";
     }
+    cout<<endl;
+    vector<int> v{1,2,3};
+    //rotate(v.begin(),v.begin()+2,v.end());
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    next_permutation(v.begin(),v.end());
+    //Returns the next greater number possible
+    for(int x:v){
+        cout<<x<<" ";
+    }
+
+
 }
